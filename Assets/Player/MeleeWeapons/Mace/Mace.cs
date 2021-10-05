58df7285a5da9ad64ef59903021cd22401354253
Adam Offord <PS23AOfford@efcts.us>
2021-10-05 10:43:03 -0400

    Added rogue and mace weapons

---
+++ Assets/Player/MeleeWeapons/Mace/Mace.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mace : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool swinging = false;
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!swinging && Input.GetMouseButtonDown(0)){
+            anim.SetTrigger("Swing");
+            anim.SetBool("Startup",true);
+        }        
+        else if (!Input.GetMouseButton(0)){
+            anim.SetBool("Swinging",false);
+            swinging = false;
+        }
+    }
+    public void startUpFinished(){
+        anim.SetBool("Swinging",true);
+        swinging = true;
+        anim.SetBool("Startup",false);
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        if (col.tag == "Enemy" && swinging){
+            print("hit");
+        }
+    }
+}
---
+++ Assets/Player/RougeWeapons/ThrowingDagger/ThrowingDagger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingDagger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D body;
+    private CircleCollider2D hitbox;
+    [SerializeField] private float speed;
+    private Vector2 directionThrown = new Vector2(1,0);
+    [SerializeField] private float lifeTime;
+    private float spinTime = 0.25f;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Awake(){
+        body = GetComponent<Rigidbody2D>();
+        hitbox = GetComponent<CircleCollider2D>();
+        anim = GetComponent<Animator>();
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.velocity = speed*directionThrown;
+        spinTime -= Time.deltaTime;
+        if (spinTime <= 0){
+            anim.SetTrigger("spin");
+        }
+    }
+    public void updateDirection(Vector2 direction){
+        directionThrown = direction;
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        if (col.tag == "Enemy"){
+            print("Hit Enemy");
+            Destroy(gameObject);
+        }
+    }
+}
