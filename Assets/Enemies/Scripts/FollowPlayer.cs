d8e099f163fdde31572386d1a440289f6c509fe1
Adam Offord <PS23AOfford@efcts.us>
2021-10-19 10:40:07 -0400

    stuff

---
+++ Assets/Enemies/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent enemy;
+
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(player.transform.position);
+    }
+}
