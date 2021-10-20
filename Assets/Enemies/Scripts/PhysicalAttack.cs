240b36e4db1deb802f9249cdb859b47ce78017c1
Adam Offord <PS23AOfford@efcts.us>
2021-10-20 10:34:22 -0400

    Slime Enemy

---
+++ Assets/Enemies/Scripts/PhysicalAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalAttack : MonoBehaviour
+{
+    private FollowPlayer ai;
+    private Animator anim;
+    private BoxCollider2D hitbox;
+    
+    public float cooldown = 5;
+    private float attackCooldown = 0;
+
+    void Start()
+    {
+        ai = GetComponent<FollowPlayer>();
+        anim = GetComponent<Animator>();
+        hitbox = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ai.stopped && !ai.attacking && attackCooldown <= 0) {
+            ai.attacking = true;
+            anim.SetTrigger("attack");
+        }
+        attackCooldown -= Time.deltaTime;
+
+    }
+    public void attackStart() {
+        hitbox.enabled = true;
+    }
+    public void attackEnd() {
+        hitbox.enabled = false;
+        ai.attacking = false;
+        attackCooldown = cooldown;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") {
+            print("HIT");
+        }
+    }
+}
