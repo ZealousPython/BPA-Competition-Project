da4ad7b3c73642328174d111023e11aaf71f205f
Adam Offord <PS23AOfford@efcts.us>
2021-10-21 10:40:57 -0400

    Did things

---
+++ Assets/MiscScripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    private int level = 1;
+
+    public int gold = 0;
+    public int playerClass = 0; // 0 is warrior, 1 is rouge, 2 is mage
+    public float playerMaxHealth = 6;
+    public float playerHealth = 3;
+    public float playerMaxMana = 100;
+    public float playerMana = 100;
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+    void Update()
+    {
+
+    }
+}
