81115008ed423fea2eca1941ef38e37c2aae975a
MegaAdamMan <40010857+MegaAdamMan@users.noreply.github.com>
2021-10-21 12:50:35 -0400

    stuff

---
+++ Assets/Enemies/Scripts/EnemyHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health = 3;
+    public void hit(float damage) {
+        health -= damage;
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
