cbd42c3eed3eceaa9caf243e2970ae3e8eea4c6e
Adam Offord <PS23AOfford@efcts.us>
2021-11-19 13:06:57 -0500

    kljhgfd

--- Assets/levels/LastBoss/Wind/ProjectileHolder.cs
+++ Assets/levels/LastBoss/Wind/ProjectileHolder.cs
@@ -7,8 +7,6 @@ public class ProjectileHolder : EnemyProjectile
     // Start is called before the first frame update
     public GameObject[] Projectiles;
     new public void updateDirection(Vector2 direction) {
-        print("Hi");
-        print(direction);
         for (int i = 0; i < Projectiles.Length; i++) {
             Projectiles[i].GetComponent<EnemyProjectile>().updateDirection(direction);
             
