101417bd82305e4c66b2d21305622f1852317b33
Adam Offord <PS23AOfford@efcts.us>
2021-12-02 12:51:45 -0500

    kjhgfdxcfvgbnm

---
+++ Assets/UI/MageShopUIUpdater.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MageShopUIUpdater : MonoBehaviour
+{
+    public Button UpgradeMageBasicAttack;
+    public Text MageBasicAttackPrice;
+    public Text RockPrice;
+    public Text FirePrice;
+    public Text IcePrice;
+    private GameManager game;
+    public Sprite[] UpgradeUI;
+    private MageBasicAttack Mage;
+
+    void Awake() {
+        
+        
+    }
+
+    void Start() {
+        game = GameManager.instance;
+        Mage = game.player.GetComponent<MageBasicAttack>();
+       
+    }
+    void Update() {
+        RockPrice.text = Mage.rockCast.price.ToString();
+        FirePrice.text = Mage.fireCast.price.ToString();
+        IcePrice.text = Mage.iceCast.price.ToString();
+        MageBasicAttackPrice.text = ((int)Mage.basicAttack.GetComponent<ProjectileMovement>().level * 25).ToString();
+    }
+    public void MageUpgradeButtonPressed() {
+        MageBasicAttackPrice.text = ((int)game.player.GetComponent<MageBasicAttack>().basicAttack.GetComponent<ProjectileMovement>().level * 25).ToString();
+        UpgradeMageBasicAttack.image.sprite = UpgradeUI[Mage.basicAttack.GetComponent<ProjectileMovement>().level-1];
+    }
+}
