0156f79225302347955f5abc52e3edcf5744b907
Adam Offord <PS23AOfford@efcts.us>
2021-12-03 12:51:36 -0500

    Zxzxcz

--- Assets/Player/Scripts/WarriorWeapons.cs
+++ Assets/Player/Scripts/WarriorWeapons.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RogueAttack : MonoBehaviour
+public class WarriorWeapons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private GameObject weapon;
-    private float coolDown;
-    private float currentCoolDown = 0;
+    private GameObject currentWeapon;
     public List<GameObject> weapons;
     public List<int> weaponLevels = new List<int>();
     public int currentWeaponLevel = 1;
@@ -18,10 +15,11 @@ public class RogueAttack : MonoBehaviour
     private Vector2[] SpellUIPositions = new Vector2[] { new Vector2(-56, 8), new Vector2(-9, 10), new Vector2(34, 11) };
     public Sprite[] WeaponUISprites;
     public Image[] SpriteUIImages;
+    // Start is called before the first frame update
     void Start()
     {
-        weapon = GameManager.instance.playerWeapon;
-        
+        currentWeapon = GameManager.instance.playerWeapon;
+
         weapons = GameManager.instance.weaponsOwned;
 
         SpellBar = GameManager.instance.spellBar;
@@ -29,17 +27,17 @@ public class RogueAttack : MonoBehaviour
         for (int i = 0; i < weapons.Count; i++)
         {
             SpriteUIImages[i].enabled = true;
-            if (weapons[i].GetComponent<Weapon>().name == "Dagger")
+            if (weapons[i].GetComponent<Weapon>().name == "Sword")
                 SpriteUIImages[i].sprite = WeaponUISprites[0];
-            else if (weapons[i].GetComponent<Weapon>().name == "Javelin")
+            else if (weapons[i].GetComponent<Weapon>().name == "Mace")
             {
                 SpriteUIImages[i].sprite = WeaponUISprites[1];
-                SpriteUIImages[i].transform.localScale = new Vector3(.5f, .5f, 1);
+                SpriteUIImages[i].transform.localScale = new Vector3(1, 1, 1);
             }
-            else if (weapons[i].GetComponent<Weapon>().name == "Shurikan")
+            else if (weapons[i].GetComponent<Weapon>().name == "Spear")
             {
                 SpriteUIImages[i].sprite = WeaponUISprites[2];
-                SpriteUIImages[i].transform.localScale = new Vector3(.5f, .5f, 1);
+                SpriteUIImages[i].transform.localScale = new Vector3(1, 1, 1);
             }
             else
             {
@@ -49,18 +47,10 @@ public class RogueAttack : MonoBehaviour
             }
         }
         SpellBar.sprite = SpellBarFrames[1];
+        createWeapon();
     }
-
-    void Update()
+    private void changeWeapons()
     {
-        currentCoolDown -= Time.deltaTime;
-        if (Input.GetMouseButton(0) && currentCoolDown <= 0){
-            shoot();
-            currentCoolDown = coolDown;
-        }
-        changeWeapons();
-    }
-    private void changeWeapons() {
         if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
             weaponSlot += 1;
@@ -69,9 +59,9 @@ public class RogueAttack : MonoBehaviour
                 weaponSlot = 0;
             }
             SpellBar.sprite = SpellBarFrames[weaponSlot + 1];
-            weapon = weapons[weaponSlot];
-            coolDown = weapon.GetComponent<RougeWeapon>().cooldown;
-            currentWeaponLevel = weaponLevels[weaponSlot];
+            //currentWeaponLevel = weaponLevels[weaponSlot];
+            currentWeapon = weapons[weaponSlot];
+            swapCurrentWeapon();
         }
         else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
@@ -81,9 +71,9 @@ public class RogueAttack : MonoBehaviour
                 weaponSlot = weapons.Count - 1;
             }
             SpellBar.sprite = SpellBarFrames[weaponSlot + 1];
-            weapon = weapons[weaponSlot];
-            coolDown = weapon.GetComponent<RougeWeapon>().cooldown;
-            currentWeaponLevel = weaponLevels[weaponSlot];
+            //currentWeaponLevel = weaponLevels[weaponSlot];
+            currentWeapon = weapons[weaponSlot];
+            swapCurrentWeapon();
         }
     }
     public void updateSpellUI()
@@ -106,17 +96,22 @@ public class RogueAttack : MonoBehaviour
             }
         }
     }
-    private void shoot(){
-        Vector3 rotation = transform.rotation.eulerAngles;
-        rotation = new Vector3(rotation.x, rotation.y, rotation.z + 180);
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = (mousePos - transform.position);
-        direction.z = 0.0f;
-        Vector3 directionNormalized = direction.normalized;
-        GameObject p = (GameObject)Instantiate(weapon, transform.position, Quaternion.Euler(rotation));
-        RougeWeapon pscript = p.GetComponent<RougeWeapon>();
-        pscript.level = currentWeaponLevel;
-        pscript.updateProjectileLevel();
-        pscript.updateDirection(new Vector2(directionNormalized.x,directionNormalized.y));
+    private void swapCurrentWeapon() {
+        transform.GetChild(0).GetComponent<Weapon>().destroyWeapon();
+        GameObject playerWeapon = (GameObject)Instantiate(currentWeapon, transform.position, transform.rotation);
+        playerWeapon.GetComponent<Weapon>().level = currentWeaponLevel;
+        playerWeapon.transform.parent = gameObject.transform;
+        GameManager.instance.playerWeapon = currentWeapon;
+    }
+    public void createWeapon() {
+        GameObject playerWeapon = (GameObject)Instantiate(currentWeapon, transform.position, transform.rotation);
+        playerWeapon.GetComponent<Weapon>().level = currentWeaponLevel;
+        playerWeapon.transform.parent = gameObject.transform;
+        GameManager.instance.playerWeapon = currentWeapon;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        changeWeapons();
     }
 }
