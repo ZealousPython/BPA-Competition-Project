3e9fb69f247d42bde8564cfc017145d3cd349240
Adam Offord <PS23AOfford@efcts.us>
2021-12-06 10:43:24 -0500

    xcvbbf

--- Assets/UI/RougeShopUIUpdater.cs
+++ Assets/UI/RougeShopUIUpdater.cs
@@ -31,8 +31,11 @@ public class RougeShopUIUpdater : MonoBehaviour
         else
             DaggerPrice.text = "";
         UpgradeDagger.image.sprite = UpgradeUI[game.weaponLevels[0] - 1];
-        JavelinPrice.text = ((int)JavelinWeapon.GetComponent<Weapon>().price).ToString();
-        ShurikanPrice.text = ((int)ShurikanWeapon.GetComponent<Weapon>().price).ToString();
+        if (game.weaponsOwned.Count <= 1)
+        {
+            JavelinPrice.text = ((int)JavelinWeapon.GetComponent<Weapon>().price).ToString();
+            ShurikanPrice.text = ((int)ShurikanWeapon.GetComponent<Weapon>().price).ToString();
+        }
         if (game.weaponsOwned.Count == 2)
         {
             if (game.weaponsOwned[1].name == "Javelin") {
@@ -76,7 +79,28 @@ public class RougeShopUIUpdater : MonoBehaviour
                 }
                 else
                     ShurikanPrice.text = "";
+
                 UpgradeShurikan.image.sprite = UpgradeUI[game.weaponLevels[2] - 1];
+            }
+            if (game.weaponsOwned[1].name == "Javelin")
+            {
+                if (game.weaponLevels[1] != 3)
+                {
+                    JavelinPrice.text = ((int)JavelinWeapon.GetComponent<Weapon>().price * game.weaponLevels[1]).ToString();
+                }
+                else
+                    JavelinPrice.text = "";
+                UpgradeJavelin.image.sprite = UpgradeUI[game.weaponLevels[1] - 1];
+            }
+            else if (game.weaponsOwned[1].name == "Shurikan")
+            {
+                if (game.weaponLevels[1] != 3)
+                {
+                    ShurikanPrice.text = ((int)ShurikanWeapon.GetComponent<Weapon>().price * game.weaponLevels[1]).ToString();
+                }
+                else
+                    ShurikanPrice.text = "";
+                UpgradeShurikan.image.sprite = UpgradeUI[game.weaponLevels[1] - 1];
             }
         }
     }
