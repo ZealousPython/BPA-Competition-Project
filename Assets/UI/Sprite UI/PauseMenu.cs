d89dc3d128ba0fcf2bc985d573d6a0843318ff5e
ps23aofford <ps23aofford@efcts.us>
2021-12-13 10:43:41 -0500

    AASDASD

---
+++ Assets/UI/Sprite UI/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void pause() {
+        GameManager.instance.unPause();
+    }
+    public void quit() {
+        GameManager.instance.Quit();
+    }
+    public void save() {
+        GameManager.instance.saveFile();
+    }
+    public void kill() {
+        Destroy(gameObject);
+    }
+}
